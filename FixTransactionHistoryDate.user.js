a9f2b78c89173b84f883b4349e5e1d8174034170
Ray Silent <ray.silent@gmail.com>
2018-03-15 14:21:23 -0400

    L$ Transaction History Today Button

--- FixTransactionHistoryDate.user.js
+++ FixTransactionHistoryDate.user.js
@@ -1,19 +1,89 @@
 // ==UserScript==
-// @name        L$ Transaction History Date Fix
+// @name        L$ Transaction Today Button
 // @namespace   accounts.secondlife.com
-// @description L$ Transaction History Date Fix From Local To SLT
+// @description L$ Transaction History Today Button
 // @match       https://accounts.secondlife.com/transaction_history*
 // @include     https://accounts.secondlife.com/transaction_history*
 // @grant       none
-// @copyright   2014, Ray Silent
+// @copyright   2014-2018, Ray Silent
+// @run-at      document-idle
 // ==/UserScript==
 
 // By Ray Silent
 
-var run = false;
+const fixDates = false;
+
+function todayButton() {
+  buttons = document.querySelectorAll("button");
+  for ( var i = 0; i < buttons.length; i++ ) {
+    if (buttons[i].textContent == "View") {
+      var node = buttons[i];
+      if (node.parentNode) {
+        // remove a node from the tree, unless 
+        // it's not in the tree already
+        var clone = node.cloneNode(true);
+        clone.textContent = "Today";
+        //var today = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+
+        clone.setAttribute('onClick', `
+        var today = new Date();
+        controllerElement = document.querySelectorAll("div[ng-controller='TransactionsCtrl']");
+        //document.getElementById('startDate').value=today.getFullYear()+"/"+(today.getMonth()<9?'0':'')+(today.getMonth()+1)+"/"+(today.getDate()<10?'0':'')+today.getDate();
+        var scope = angular.element(controllerElement).scope();
+        if (scope) {
+          scope.$apply(function() {
+            scope.history.startDate = moment(scope.history.now).startOf("day");
+            scope.history.endDate = moment(scope.history.now).endOf("day");
+            scope.fetchHistory();
+            scope.showAll();
+          });
+          
+        }
+        `);
+        var todayNode = node.parentNode.appendChild(clone);
+
+        clone = node.cloneNode(true);
+        clone.textContent = "<";
+        clone.setAttribute('onClick', `
+        controllerElement = document.querySelectorAll("div[ng-controller='TransactionsCtrl']");
+        var scope = angular.element(controllerElement).scope();
+        if (scope) {
+          scope.$apply(function() {
+            scope.history.startDate = scope.history.startDate.subtract(1, "days");
+            scope.history.endDate = scope.history.endDate.subtract(1, "days");
+            scope.fetchHistory();
+            scope.showAll();
+          });
+        }
+        `);
+        node.parentNode.insertBefore(clone, todayNode);
+
+        clone = node.cloneNode(true);
+        clone.textContent = ">";
+        clone.setAttribute('onClick', `
+        controllerElement = document.querySelectorAll("div[ng-controller='TransactionsCtrl']");
+        var scope = angular.element(controllerElement).scope();
+        if (scope) {
+          if (scope.history.startDate.diff(moment(scope.history.now).startOf("day"), 'days')<0
+              && scope.history.endDate.diff(moment(scope.history.now).endOf("day"), 'days')<0) {
+            scope.$apply(function() {
+              scope.history.startDate = scope.history.startDate.add(1, "days");
+              scope.history.endDate = scope.history.endDate.add(1, "days");
+              scope.fetchHistory();
+              scope.showAll();
+            });
+          }
+        }
+        `);
+        node.parentNode.insertBefore(clone, null);
+
+      }
+    }
+  }
+}
 
 function updateTransactionTimestamps() {
-  if (run) return;
+  
   
   ngBindingElements = document.getElementsByClassName('ng-binding');
   for (var i = 0; i < ngBindingElements.length; i++) {
@@ -40,13 +110,16 @@ function updateTransactionTimestamps() {
           element.innerHTML = source+"<br/><br/>SLT:<br/>"
                    +UTCDate.toISOString().substring(0, 10)+' '
                    +UTCDate.toISOString().substring(11, 19);
+          
         }
-        
-        
       }
     }
-    
   }
 }
 
-var timerVar = setInterval( function(){ updateTransactionTimestamps() }, 5000);
+function run() {
+    if (fixDates) updateTransactionTimestamps();
+    todayButton();
+}
+
+run();
--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 SL-Transaction-History-Date-Fix-Greasemonkey-Script
 ===================================================
 
-Second Life L$ Transaction History Date Fix From Local To SLT
+Current version of the script adds "Today" as well as "<" and ">" buttons to show **all** transaction history items for a single day.
 
+![How it works](https://i.imgur.com/lohF7iP.gif)
 
+Why? Since LL started showing 7 days by default with 25 items per page, I thought it was a bit too much to always click the dates instead of just one button. Besides, clicking "Show All" every time became quite annoying. The trick is to request Angular do all necessary modifications. 
 
-That was a script to fix temporary bug in Second Life L$ Transaction History which was showing Local browser time in transactions instead of SLT time which most were used to.
-The bug is now fixed, so I'm only keeping this here for history.
+Currently it works the best with the [Violentmonkey Firefox Addon](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/).
 
+The way you add it, after installing Violentmonkey, simple go to https://github.com/raysilent/SL-Transaction-History-Date-Fix-Greasemonkey-Script/raw/master/FixTransactionHistoryDate.user.js and it will offer you to review and install the script (Confirm Installation button on top right).
 
-References:
+References
+__________
 
-https://jira.secondlife.com/browse/BUG-6335
+Originall the script aimed to fix a temporary bug that whould show Second Life L$ Transaction History in local broser time zone. The bug is now fixed.
+
+* https://jira.secondlife.com/browse/BUG-6335
